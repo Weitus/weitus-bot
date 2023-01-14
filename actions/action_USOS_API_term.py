e54822ddd110bca482a61b8a8af7b565e3d62ad9
Drajner <drajner@wp.pl>
2023-01-14 18:47:42 +0100

    added 2 not yet checked usos api searches

---
+++ actions/action_USOS_API_term.py
@@ -0,0 +1,40 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import json
+import requests
+from datetime import date, datetime
+
+
+class Action_USOS_API_term(Action):
+
+    def name(self) -> Text:
+        return "action_USOS_API_term"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        current_year = date.today().year
+        current_year = str(current_year) + "L"
+        base_url = f"https://apps.usos.pw.edu.pl/services/terms/term?term_id={current_year}"
+        term_request = requests.post(base_url)
+        term_request_dict = json.loads(term_request.text)
+        term_start_date = datetime.strptime(term_request_dict["start_date"], '%Y-%m-%d')
+        term_end_date = datetime.strptime(term_request_dict["end_date"], '%Y-%m-%d')
+        is_before_start = term_start_date > datetime.today()
+        is_after_finish = term_end_date < datetime.today()
+        if is_before_start:
+            current_year = str(date.today().year - 1) + "Z"
+        elif is_after_finish:
+            current_year = current_year[:-1] + "Z"
+        term_request = requests.post(base_url)
+        term_start_date = datetime.strptime(term_request_dict["start_date"], '%Y-%m-%d').date()
+        term_end_date = datetime.strptime(term_request_dict["end_date"], '%Y-%m-%d').date()
+        response = f"Term id is {current_year}. It started on {term_start_date} and it ends on {term_end_date}."
+
+        dispatcher.utter_message(text=str(response))
+
+        return []
---
+++ actions/action_USOS_API_theses_search.py
@@ -0,0 +1,57 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from usos_credentials import usos_key, usos_secret
+
+import json
+import requests
+import oauth2
+
+
+class Action_USOS_API_theses_search(Action):
+
+    def name(self) -> Text:
+        return "action_USOS_API_theses_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        input_prompt = tracker.latest_message["text"]
+        base_url = "https://apps.usos.pw.edu.pl/services/theses/search?lang=en"
+        base_url2 = "https://apps.usos.pw.edu.pl/services/theses/thesis?ths_id="
+        request_url = base_url + "&query=" + input_prompt
+        consumer = oauth2.Consumer(usos_key,usos_secret)
+        client = oauth2.Client(consumer)
+        thesis_response = client.request(request_url, "POST")
+        response = str(thesis_response[1])[2:-1]
+        response_dict = json.loads(response)
+        #response = "This are theses that I found in PW database with help of USOS: \n"
+        bot_message = ''
+        for hit in response_dict["items"]:
+            ths_id = hit["thesis"]['id']
+            request_url = base_url2 + str(ths_id) + "&fields=title|authors|supervisors"
+            thesis_response = client.request(request_url, "POST")
+            response = str(thesis_response[1])[2:-1]
+            response_dict2 = json.loads(response)
+            bot_message += response_dict2['title']
+            bot_message += ' by '
+            authors_text = ''
+            for a in response_dict2['authors']:
+                authors_text += a['first_name'] + ' ' + a['last_name'] + ' '
+            if authors_text == '':
+                authors_text = 'unknown author '
+            bot_message += authors_text
+            bot_message += 'subervised by '
+            supervisors_text = ''
+            for a in response_dict2['supervisors']:
+                supervisors_text += a['first_name'] + ' ' + a['last_name'] + ' '
+            if supervisors_text == '':
+                supervisors_text = 'unknown supervisors '
+            bot_message += supervisors_text
+            bot_message += '\n'
+        
+        dispatcher.utter_message(text=str(response))
+
+        return []
